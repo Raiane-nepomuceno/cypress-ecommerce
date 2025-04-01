ff8e032524f0669f062178d028f9e7cd74942496
Raiane-nepomuceno <nepomucenoraiane@gmail.com>
2025-04-01 16:43:49 -0300

    Corrigir configuração do CYPRESS_RECORD_KEY no workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId:'zex6bm',
+  viewportHeight: 880,
+  viewportWidth: 1280,
+
   e2e: {
     baseUrl: 'https://ecommerce-playground.lambdatest.io/',
     setupNodeEvents(on, config) {
